ec0f25d992b2d266d626b1f50908c15606b5eea3
smoqadam <phpro.ir@gmail.com>
2017-08-05 08:14:24 +0430

    remove JMS annotations

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -12,12 +12,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use JMS\Serializer\Annotation as JMSSerializer;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
- * @JMSSerializer\ExclusionPolicy("all")
  */
 class User extends BaseUser
 {
